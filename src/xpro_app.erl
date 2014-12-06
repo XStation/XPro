4c8da6dbaaf77ac7d18bcd060aa9a8d88932639a
ronalfei <ronalfei@qq.com>
2014-12-06 16:38:06 +0800

    support priv dir

--- src/xpro_app.erl
+++ src/xpro_app.erl
@@ -1,10 +1,28 @@
 -module(xpro_app).
 -behaviour(application).
 
--export([start/2]).
+-export([start/0,start/2]).
 -export([stop/1]).
 
+
+start() ->
+	application:start(xpro).
 start(_Type, _Args) ->
+	lager:start(),
+	application:start(sasl),
+	application:start(crypto),
+	application:start(ranch),
+	application:start(cowlib),
+	application:start(cowboy),
+
+	Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/xchat", xchat_websocket_handler, []},
+			{"/[...]", cowboy_static, {priv_dir, 'XPro', <<"">>}}
+		]}
+	]),
+	{ok, _} = cowboy:start_http(http, 10, [{port, 8080}],
+		[{env, [{dispatch, Dispatch}]}]),
 	xpro_sup:start_link().
 
 stop(_State) ->
