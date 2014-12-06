01c6e034cd453ad9e175c225fabb234a3f0da240
ronalfei <ronalfei@qq.com>
2014-12-06 16:30:59 +0800

    fix path

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ROOT=`pwd`
 EBIN=$ROOT"/ebin"
-erl -boot start_sasl -sname xpro@localhost -pa $EBIN -pa $ROOT"/deps/*/ebin" -s xpro_app -s reloader +K true -setcookie xpro \
+erl -boot start_sasl -sname xpro@localhost -pa $EBIN -pa $ROOT/deps/*/ebin -s xpro_app -s reloader +K true -setcookie xpro \
     -eval "io:format(\"* xpros service already started~n~n\")."
