ce502548c4623da43a4aec8d7cfbae76d7a3df92
ronalfei <ronalfei@qq.com>
2014-12-06 17:40:12 +0800

    add xchat_ws_handler

--- src/xchat_websocket_handler.erl
+++ src/xchat_websocket_handler.erl
@@ -1,24 +1,72 @@
--module(xchart_websocket_handler).
+-module(xchat_websocket_handler).
 -include("lager.hrl").
+%-behaviour(cowboy_websocket_handler).
 
-
--export([init/2]).
+-export([init/3]).
+-export([websocket_init/3]).
 -export([websocket_handle/3]).
 -export([websocket_info/3]).
+-export([websocket_terminate/3]).
 
-init(Req, Opts) ->
-	lager:error("self pid ~p", [self()]),
-	erlang:start_timer(1000, self(), <<"Hello!">>),
-	{cowboy_websocket, Req, Opts}.
 
-websocket_handle({text, Msg}, Req, State) ->
-	{reply, {text, << "That's what she said! ", Msg/binary >>}, Req, State};
+
+-export([send_self/1]).
+
+init({tcp, http}, _Req, _Opts) ->
+	{upgrade, protocol, cowboy_websocket}.
+
+websocket_init(_TransportName, Req, _Opts) ->
+	erlang:start_timer(0, self(), <<"HI">>),
+	lager:warning("self:~p", [self()]),
+	{ok, Req, {} }.
+
+
+websocket_handle({text, <<"SEND#", ToUserid/binary>>}, Req, State) ->
+    case pid_manager:get_pid(<<"RECEIVE#", ToUserid/binary>>) of 
+        {ok, Pid} ->
+            NewState = {<<"SEND#">>, Pid},
+            pid_manager:add_pid({NewState, self()}),%% 把发送流的进程插入到ets, 目前没有太大意义
+lager:warning("111111111: ~p", [NewState]),
+	        {reply, {text, <<"GO">>}, Req, NewState};
+        {error, Reason} ->
+            {reply, {text, Reason}, Req, State}
+    end;
+
+websocket_handle({text, Msg}, Req, {<<"SEND#">>, ToPid}) ->
+lager:warning("msg to Pid:~p", [ToPid]),
+    ToPid ! {text, Msg},
+	{ok, Req, {<<"SEND#">>, ToPid}};
+
+
+websocket_handle({binary, Bin}, Req, {<<"SEND#">>, ToPid}) ->
+    ToPid ! {binary, Bin},
+lager:warning("binary to Pid:~p", [ToPid]),
+	{ok, Req, {<<"SEND#">>, ToPid}};
+
+websocket_handle({binary, _Bin}, Req, State) ->
+	{ok, Req, State};
+
 websocket_handle(_Data, Req, State) ->
+lager:warning("any data :~p", [_Data]),
 	{ok, Req, State}.
+
+
+
 
 websocket_info({timeout, _Ref, Msg}, Req, State) ->
-	erlang:start_timer(1000, self(), <<"How' you doin'?">>),
 	{reply, {text, Msg}, Req, State};
+
+
+websocket_info({text, Msg}, Req, State) ->
+	{reply, {text, Msg}, Req, State};
+
 websocket_info(_Info, Req, State) ->
-	lager:warning("receive other info: ~p", [_Info]),
 	{ok, Req, State}.
+
+websocket_terminate(_Reason, _Req, _State) ->
+	ok.
+
+
+send_self(Pid) ->
+	Pid ! {text, <<"Iam god!">>}.
+	
