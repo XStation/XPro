b5e44e52f0f93867498f66c9edc691320359c55c
ronalfei <ronalfei@qq.com>
2014-12-11 16:20:55 +0800

    fill code, format code

--- src/xchat_websocket_handler.erl
+++ src/xchat_websocket_handler.erl
@@ -1,7 +1,11 @@
 -module(xchat_websocket_handler).
 -include("lager.hrl").
-%-behaviour(cowboy_websocket_handler).
 
+
+%% API
+-export([send_self/1]).
+
+%% websocket handler
 -export([init/3]).
 -export([websocket_init/3]).
 -export([websocket_handle/3]).
@@ -9,83 +13,87 @@
 -export([websocket_terminate/3]).
 
 
+-record(state, {
+	xnest_name = <<>> :: binary(),
+	xnest_pid  = <<>> :: pid()
+}).
 
--export([send_self/1]).
 
-
+%% type()
 -type req() :: cowboy_http:req().
 -export_type([req/0]).
 
+
+
+%% @private
 init({tcp, http}, _Req, _Opts) ->
 	{upgrade, protocol, cowboy_websocket}.
 
+%% @private
 websocket_init(_TransportName, Req, _Opts) ->
-	%erlang:start_timer(0, self(), <<"HI">>),
 	{XNestName, NewReq} = parse_xnest_name(Req),
-	join_xnest(XNestName),
-	{ok, NewReq, {} }.
+	{ok, XNestPid} = join_xnest(XNestName),
+	State = #state{
+		xnest_name	= XNestName,
+		xnest_pid	= XNestPid
+	},
+	{ok, NewReq, State }.
 
 
-websocket_handle({text, <<"SEND#", ToUserid/binary>>}, Req, State) ->
-    case pid_manager:get_pid(<<"RECEIVE#", ToUserid/binary>>) of 
-        {ok, Pid} ->
-            NewState = {<<"SEND#">>, Pid},
-            pid_manager:add_pid({NewState, self()}),%% 把发送流的进程插入到ets, 目前没有太大意义
-lager:warning("111111111: ~p", [NewState]),
-	        {reply, {text, <<"GO">>}, Req, NewState};
-        {error, Reason} ->
-            {reply, {text, Reason}, Req, State}
-    end;
 
-websocket_handle({text, Msg}, Req, {<<"SEND#">>, ToPid}) ->
-lager:warning("msg to Pid:~p", [ToPid]),
-    ToPid ! {text, Msg},
-	{ok, Req, {<<"SEND#">>, ToPid}};
-
-
-websocket_handle({binary, Bin}, Req, {<<"SEND#">>, ToPid}) ->
-    ToPid ! {binary, Bin},
-lager:warning("binary to Pid:~p", [ToPid]),
-	{ok, Req, {<<"SEND#">>, ToPid}};
-
-websocket_handle({binary, _Bin}, Req, State) ->
+%% @private
+%% @doc Receive Message from client and send it to XNest
+websocket_handle({text, Msg}, Req, State ) ->
+	XNestPid = State#state.xnest_pid,
+    XNestPid ! {self(), {text, Msg}},
 	{ok, Req, State};
 
+%% @private
+%% @doc Receive Message from client and send it to XNest, But unused
+websocket_handle({binary, Bin}, Req, State) ->
+	XNestPid = State#state.xnest_pid,
+    XNestPid ! {self(), {binary, Bin}},
+	{ok, Req, State};
+
+%% @private
 websocket_handle(_Data, Req, State) ->
-lager:warning("any data :~p", [_Data]),
-	lager:warning("req:~p", [Req]),
 	{ok, Req, State}.
 
 
+%% @private
+%% @doc Receive Message from xnest and send it to client
+websocket_info({FromPid, {text, Msg}}, Req, State) ->
+	ResponseMsg  = {{<<"from">>, FromPid}, {<<"msg">>, Msg}},
+	{reply, {text, ResponseMsg}, Req, State};
 
-
+%% @private
+%% @doc Receive timeout event , But now unused
 websocket_info({timeout, _Ref, Msg}, Req, State) ->
 	{reply, {text, Msg}, Req, State};
 
-
-websocket_info({text, Msg}, Req, State) ->
-	{reply, {text, Msg}, Req, State};
-
+%% @private
 websocket_info(_Info, Req, State) ->
 	{ok, Req, State}.
 
+%% @private
 websocket_terminate(_Reason, _Req, _State) ->
 	ok.
 
 
 %%===================Internal functions===============
 
-
+%% @doc Parse xnest Name from HTTP Req
 -spec parse_xnest_name(req()) -> {binary(), req()}.
 parse_xnest_name(Req) ->
 	cowboy_req:qs_val(<<"xnest">>, Req, <<>>).
 
-
--spec join_xnest(binary()) -> ok.
+%% @doc Join a xnest 
+-spec join_xnest(binary()) -> {ok, pid()}.
 join_xnest(XNestName) ->
-	lager:info("~p", [XNestName]),
-	ok.
+	{ok, Pid} = xnest_manager:join_xnest(XNestName),   %% I can know what format will be return by xnest_manager
+	{ok, Pid}.
 
+
+%% @doc A test function 
 send_self(Pid) ->
 	Pid ! {text, <<"Iam god!">>}.
-	
