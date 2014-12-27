aceca5c827f2b8fbb22053c58f94f3c4f8ec2720
ronalfei <ronalfei@qq.com>
2014-12-27 19:02:45 +0800

    add curl scripts for http request

---
+++ scripts/test/history.sh
@@ -0,0 +1,1 @@
+curl http://localhost:8080/xgate/http -d "{'request_id':'111', 'method':'history', 'params':{'xnest':'test','count':'4'}}" -v
---
+++ scripts/test/members.sh
@@ -0,0 +1,1 @@
+curl http://localhost:8080/xgate/http -d "{'request_id':'111', 'method':'members', 'params':{'xnest':'test'}}" -v
