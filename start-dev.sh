be77a580c8c8a764642d005c85b1ae83d6f5bff7
ronalfei <ronalfei@qq.com>
2015-01-03 10:24:12 +0800

    change erlang port limit

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ROOT=`pwd`
 EBIN=$ROOT"/ebin"
-erl -boot start_sasl -sname xpro@localhost -pa $EBIN -pa $ROOT/deps/*/ebin -s xpro_app -s reloader +K true -setcookie xpro \
+erl -boot start_sasl -sname xpro@localhost -pa $EBIN -pa $ROOT/deps/*/ebin -s xpro_app -s reloader \
+	 +K true -P 134217727 -Q 134217727 -env  ERL_MAX_PORTS 409600  -setcookie xpro \
     -eval "io:format(\"* xpros service already started~n~n\")."
