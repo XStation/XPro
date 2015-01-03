0a6a27e224e5a8e564972096789a40672425dded
ronalfei <ronalfei@qq.com>
2015-01-03 10:28:43 +0800

    add analyze for percept2

--- src/libs/analyze.erl
+++ src/libs/analyze.erl
@@ -1,15 +1,22 @@
 -module(analyze).
 
--export([start/0, stop/0]).
+-export([start/0, stop/0, sample/0, do/0]).
 
+-define(DATAFILE, "/dev/shm/percept2.txt").
 %http://refactoringtools.github.io/percept2/
 %trace_profile_option() = procs_basic | ports_basic | procs | ports | schedulers | running | message | migration | garbage_collection | s_group | all | {callgraph, [module_name()]}
 
+
 start() ->
-	File = "/dev/shm/percept2.txt",
-	percept2:profile(File, [procs_basic, message,{callgraph, [xnest, xnest_manager]}]).
+	percept2:profile(?DATAFILE, [procs_basic, message, procs, {callgraph, [xnest, xnest_manager, ets]}]).
+
 stop() ->
-	File = "/dev/shm/percept2.txt",
-	percept2:stop_profile(),
-	percept2:analyze([File]),
+	percept2:stop_profile().
+
+do() ->
+	percept2:analyze([?DATAFILE]),
 	percept2:start_webserver(8888).
+
+sample() ->
+	percept2_sampling:start([all], 60000, ".").
+
