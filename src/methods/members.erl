40de23ed752ec43d19eedc5e2314ef72513dae66
ronalfei <ronalfei@qq.com>
2015-01-20 11:29:45 +0800

    add pid2binary

--- src/methods/members.erl
+++ src/methods/members.erl
@@ -16,9 +16,14 @@
 %% @private
 -spec do(props()) -> term().
 do(Params) ->
-lager:info("before xnestname :~p", [Params]),
 	XNestName = proplists:get_value(<<"xnest">>, Params),
 lager:info("xnestname :~p", [XNestName]),
         {ok, XNestPid} = xnest_manager:get_xnest(XNestName),
         {ok, Members} = xnest:members(XNestPid),
-        Members.
+		[[pid2binary(Pid), Name] || {Pid, Name} <- Members].
+
+
+
+%% @doc pid to binary
+-spec pid2binary(pid()) -> binary().
+pid2binary(Pid) -> list_to_binary(pid_to_list(Pid)).
