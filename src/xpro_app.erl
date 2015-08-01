34a2a529a640cc640c087fc0e71342cf674de489
Xin Zhao <zhaoxina@gmail.com>
2015-08-01 08:38:30 +0800

    modify path from absolute to relative

--- src/xpro_app.erl
+++ src/xpro_app.erl
@@ -25,14 +25,14 @@ start(_Type, _Args) ->
 	{ok, _} = cowboy:start_http(http, 10, [{port, 8080}],
 		[{env, [{dispatch, Dispatch}]}]),
 
-	PrivDir = code:priv_dir(xpro),
-	{ok, _} = cowboy:start_https(https, 10, 
-				[{port, 443}
-				, {cacertfile, PrivDir ++ "/../ssl/cowboy-ca.crt"}
-				, {certfile, PrivDir ++ "/../ssl/server.crt"}
-				, {keyfile, PrivDir ++ "/../ssl/server.key"}],
-				[{env, [{dispatch, Dispatch}]}]
-			),
+	%%PrivDir = code:priv_dir(xpro),
+	%%{ok, _} = cowboy:start_https(https, 10, 
+	%%			[{port, 443}
+	%%			, {cacertfile, PrivDir ++ "/../ssl/cowboy-ca.crt"}
+	%%			, {certfile, PrivDir ++ "/../ssl/server.crt"}
+	%%			, {keyfile, PrivDir ++ "/../ssl/server.key"}],
+	%%			[{env, [{dispatch, Dispatch}]}]
+	%%		),
 	
 
 	xpro_sup:start_link().
